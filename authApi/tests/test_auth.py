2510fd05a9d06f09df4dc267976cf03d87e2c1e9
Okusanya Damilola <okusanya.damilola@gmail.com>
2018-10-08 21:02:21 -0400

    Added new tests

--- authApi/tests/test_auth.py
+++ authApi/tests/test_auth.py
@@ -39,6 +39,16 @@ def get_user_status(self, token):
 		)
 	)
 
+def get_user_status_malformed_token(self, token):
+	return self.client.get(
+		'/api/auth/me',
+		headers=dict(
+			Authorization='Bearer' + json.loads(
+				token.data.decode()
+			)['refresh_token']
+		)
+	)
+
 class TestAuthBlueprint(BaseTestCase):
 	
 	def test_registration(self):
@@ -94,7 +104,7 @@ class TestAuthBlueprint(BaseTestCase):
 			self.assertEqual(response.status_code, 200) 
 
 	def test_non_registered_user_login(self):
-		""" Test for login of non-regsitered user"""
+		""" Test for login of non-registered user"""
 		with self.client:
 			response = login_user(self, 'fmaketouser2', '1a2e3r')
 			data = json.loads(response.data.decode())
@@ -131,6 +141,30 @@ class TestAuthBlueprint(BaseTestCase):
 			self.assertTrue(data.get('data')['admin'] is 'true')
 			self.assertTrue(data.get('data')['anonymous'] is 'false')
 			self.assertTrue(resp.status_code, 200)
+
+	def test_anonymous_user_status(self):
+		"""Test anonymous user status"""
+		with self.client:
+			response = register_user(self, 'fmaketouser', '123456', admin=True)
+			resp = get_user_status(response)
+			data = json.loads(resp.data.decode())
+			print(data)
+			self.assertTrue(data.get('status') == 'Success')
+			self.assertTrue(data.get('data') is not None)
+			self.assertTrue(data.get('data')['username'] == 'fmaketouser')
+			self.assertTrue(data.get('data')['admin'] is 'false')
+			self.assertTrue(data.get('data')['anonymous'] is 'true')
+			self.assertTrue(resp.status_code, 200)
+
+	def test_user_status_malformed_bearer_token(self):
+		""" Test for user status with malformed bearer token"""
+		with self.client:
+			response = register_user(self, 'fmaketouser', '123456', admin=True)
+			resp = get_user_status_malformed_token(response)
+			data = json.loads(resp.data.decode())
+			self.assertTrue(data['status'] == 'Fail')
+            self.assertTrue(data['message'] == 'Bearer token malformed.')
+			self.assertEqual(response.status_code, 401)
 
 if __name__ == '__main__':
 	unittest.main()
